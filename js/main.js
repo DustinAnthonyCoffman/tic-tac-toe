f6436df6e661fa35cb790a4b805cc0c0a3a13909
Dustin <dustin.anthony.coffman@gmail.com>
2019-10-25 15:02:27 -0500

    made game logic and some styling

--- js/main.js
+++ js/main.js
@@ -1,44 +1,85 @@
 /*----- constants -----*/
-//values that cannot changable
-const X = '-1'; 
-const O = '-1'
-const winnerPath = [];
-const board = 3;
+//values that cannot change
+
+const winningCombo = [
+    '1,2,3',  '1,5,9', '1,4,7', 
+    '2,5,8', '3,5,7', '3,6,9', 
+    '4,5,6', '6,5,4', '7,5,3', 
+    '7,8,9', '7,4,1', '8,5,2', 
+    '9,8,7', '9,6,3', '9,5,1'
+];
 
 /*----- app's state (variables) -----*/
 //the stuff that your storing in values, changable Data
-let blocks = 'null'
-let userChoice = [];
-let player1 = 'null';
-let player2 = 
+let turn = 'player1';
+let player1 = [];
+let player2 = [];
+
 
 /*----- cached element references -----*/
 
+
+
 /*----- event listeners -----*/
-//user clicks div, pulls value from const X
-//user clicks div, pulls value from const O
+//user clicks div, pulls value from div puts in player array
+document.getElementById('grid')
+    .addEventListener('click', function(evt) {
+        let boxValue = evt.target.textContent;
+        if (turn === 'player1') {
+            player1.push(boxValue);
+            turn = 'player2';
+            console.log(player1);
+            checkWinner(player1, player2);
+            return;
+
+        }
+        if (turn === 'player2') {
+            player2.push(boxValue);
+            turn = 'player1';
+            console.log(player1);
+            checkWinner(player1, player2);
+            return;
+        }
+    });
+
+    document.getElementById('grid')
+    .addEventListener('click', function(evt) {
+        let targetDiv = evt.target;
+        if (turn === player1) {
+           targetDiv = document.querySelector('div').style.backgroundImage = 'https://imgur.com/k7SbxY5';
+        }
+        if (turn === player2) {
+        document.querySelector('div').style.backgroundImage = 'https://imgur.com/Z9XWiPy';
+        }
+    })
 
 /*----- functions -----*/
 
 function init() {
-
+    turn = player1;
     render();
 }
 
 function render() {
-   
-    //display board
-    
-    function grid() {
-        for(let i = 0; i < board; i++) {
-           let xBox = document.createElement('div');
-            for(let j = 0; j < ??; j++) {
-                let yBox = document.createElement('div');
-                yBox *= 2 ??
-                
-                }
-        }
-  
-    }
+   //clear all values
+   player1 = [];
+   player2 = [];
+   turn = player1;
+}
 
+function checkWinner(p1, p2) {
+    
+    console.log('Player 1 boxes taken');  
+    console.log(p1.join());
+    console.log('Player 2 boxes taken');  
+    console.log(p2.join());
+    if (winningCombo.includes(p1.join())) {
+       console.log('player1wins');
+       render();
+
+   }
+    if (winningCombo.includes(p2.join())) {
+        console.log('player2wins');
+        render();
+}
 }
