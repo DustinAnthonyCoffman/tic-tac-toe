5e523dbe9e744a6b923810043c3070756b72ed14
Dustin <dustin.anthony.coffman@gmail.com>
2019-10-25 10:24:17 -0500

    First commit, set html,css,js

---
+++ js/main.js
@@ -0,0 +1,44 @@
+/*----- constants -----*/
+//values that cannot changable
+const X = '-1'; 
+const O = '-1'
+const winnerPath = [];
+const board = 3;
+
+/*----- app's state (variables) -----*/
+//the stuff that your storing in values, changable Data
+let blocks = 'null'
+let userChoice = [];
+let player1 = 'null';
+let player2 = 
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+//user clicks div, pulls value from const X
+//user clicks div, pulls value from const O
+
+/*----- functions -----*/
+
+function init() {
+
+    render();
+}
+
+function render() {
+   
+    //display board
+    
+    function grid() {
+        for(let i = 0; i < board; i++) {
+           let xBox = document.createElement('div');
+            for(let j = 0; j < ??; j++) {
+                let yBox = document.createElement('div');
+                yBox *= 2 ??
+                
+                }
+        }
+  
+    }
+
+}
